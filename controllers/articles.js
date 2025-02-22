714c4e4c30c8a0e6c0b0df4c4400c952789d3760
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2025-02-22 17:19:18 +0200

    add controller wrapper

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,71 +1,51 @@
 const { Article, addSchema } = require("../models/article");
-const { HttpError } = require("../utils");
+const { HttpError, ctrlWrapper } = require("../utils");
 
-const getAll = async (req, res, next) => {
-  try {
-    const result = await Article.find();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
+const getAll = async (req, res) => {
+  const result = await Article.find();
+  res.json(result);
 };
 
-const getById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await Article.findById(id);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const result = await Article.findById(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
   }
+  res.json(result);
 };
 
-const add = async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await Article.create(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
+const add = async (req, res) => {
+  const { error } = addSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, error.message);
   }
+  const result = await Article.create(req.body);
+  res.status(201).json(result);
 };
 
-const updateById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await Article.findByIdAndUpdate(id, req.body, { new: true });
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
+const updateById = async (req, res) => {
+  const { id } = req.params;
+  const result = await Article.findByIdAndUpdate(id, req.body, { new: true });
+  if (!result) {
+    throw HttpError(404, "Not found");
   }
+  res.status(200).json(result);
 };
 
-const deleteById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await Article.findByIdAndDelete(id);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.status(200).json({ message: "Deleted successfully" });
-  } catch (error) {
-    next(error);
+const deleteById = async (req, res) => {
+  const { id } = req.params;
+  const result = await Article.findByIdAndDelete(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
   }
+  res.status(200).json({ message: "Deleted successfully" });
 };
 
 module.exports = {
-  getAll,
-  getById,
-  add,
-  updateById,
-  deleteById
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  updateById: ctrlWrapper(updateById),
+  deleteById: ctrlWrapper(deleteById),
 };
