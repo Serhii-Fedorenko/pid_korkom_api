fb363dba5d091917757e4d88a254d2bad966556b
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2025-03-20 17:32:40 +0200

    change response in login controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -43,7 +43,14 @@ const login = async (req, res) => {
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "12h" });
   await User.findByIdAndUpdate(user._id, { token });
 
-  res.json({ token });
+  res.json({
+    user: {
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    },
+    token,
+  });
 };
 
 const getCurrent = async (req, res) => {
